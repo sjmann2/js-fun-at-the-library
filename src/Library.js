3898d2e7bb8e2a4df16f6e8cdce21acd95f65ec5
Sid Mann <sjmann2@outlook.com>
2023-02-03 20:40:56 -0600

    Passing tests for library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,25 @@
+function createLibrary(name){
+  return {name, shelves: {fantasy: [], fiction: [], nonFiction: []}};
+}
+function addBook(library, book){
+  library.shelves[book.genre].push(book)
+}
 
+function checkoutBook(library, bookTitle, bookGenre){
+  const genreShelf = library.shelves[bookGenre]
+  const foundTitle = genreShelf.find(element => element.title === bookTitle)
+  let indexOfBook = genreShelf.indexOf(foundTitle)
+  if (indexOfBook > -1){
+    genreShelf.splice(indexOfBook, 1)
+    return `You have now checked out ${bookTitle} from the ${library.name}`;
+  }
+  else{
+    return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+  }
+};
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
