4fe38ae5b50ea3c94230de8aeec0e86d27eb4d7b
Sid Mann <sjmann2@outlook.com>
2023-02-03 19:41:49 -0600

    Passing tests for shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,40 @@
+function shelfBook(book, shelf){
+  if (shelf.length < 3){
+    return(shelf.unshift(book))};
+}
+
+function unshelfBook(bookTitle, shelf){
+  for (i = 0; i < shelf.length; i++){
+    if (shelf[i].title === bookTitle){
+      shelf.splice(i, 1);
+      break;
+    }
+  }
+}
+
+function listTitles(shelf){
+  let titles = []
+  for (i = 0; i < shelf.length; i++){
+    titles.push(shelf[i].title);
+  }
+  return titles.join(", ");
+}
+
+function searchShelf(shelf, bookTitle){
+  let containsTitle = false
+  for (i = 0; i < shelf.length; i++){
+    if (shelf[i].title === bookTitle){
+      containsTitle = true;
+    }
+  }
+  return containsTitle;
+}
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
